4cc32f0984bf2d820b200436fed2e0d4ea286613
Andreas Schmid <andreas.schmid.as3@roche.com>
2023-02-21 23:32:01 +0100

    LSP: Enforce default keybinding as defined by lsp-zero

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,9 @@
 -- Learn the keybindings, see :help lsp-zero-keybindings
 -- Learn to configure LSP servers, see :help lsp-zero-api-showcase
 local lsp = require('lsp-zero')
-lsp.preset('recommended')
+lsp.preset({
+  name = 'recommended',
+  set_lsp_keymaps = {preserve_mappings = false},  -- Enforce overriting of keybindings
+})
 
 lsp.setup()
